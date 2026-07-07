26b8a74d225d5cd0478243a32f9652e26635e5d4
Your Name <you@example.com>
2026-07-07 18:58:58 +0200

    f

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Dmitrii Naumenko";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+	"Personal blog of Dmitrii Naumenko, a Senior Software Engineer and Cloud Architect writing about PostgreSQL, Django, and building reliable, high-performance backend systems.";
